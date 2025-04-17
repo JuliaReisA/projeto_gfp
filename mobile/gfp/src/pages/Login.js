a91bf666c98a5798958b9d810a87731b9d93c2eb
Julia <jujualmeidareis25@gmail.com>
2025-04-17 15:21:00 -0300

    Criação e configuração do projeto GFP

---
+++ mobile/gfp/src/pages/Login.js
@@ -0,0 +1,13 @@
+import { View, Text, Button} from "react-native";
+
+export default function Login({navigation}){
+
+    return (
+        <View>
+            <Text>Login</Text>
+            <Button title="Entrar"
+            onPress={() => navigation.navigate('MenuDrawer')} />
+        </View>
+          
+    )
+}
